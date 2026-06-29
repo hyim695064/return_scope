0c8fd4296a595273f518c2eda5daaed6b16b5ce8
moshe <hyim695064@gmail.com>
2026-06-29 11:04:38 +0300

    part1s2

---
+++ return.py
@@ -0,0 +1,20 @@
+# score = 10
+# #מדפיס גם 10 ןגם 7 פעם אחת את מה שבתוך הפונקציה ןפעם אחת את מה שבחוץ
+# def update_score():
+#     score = 5
+#     score = score + 2
+#     print(score)
+# update_score()
+# print(score)
+name = "agent"
+level = 2
+
+def show_info():
+    name = "spy"
+    level =4
+    power = level * 10
+    print(name)
+    print(power)
+show_info()
+print(name)
+print(level)
