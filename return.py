ff016a904af01a211d8251dbbb3cec51f544ad32
moshe <hyim695064@gmail.com>
2026-06-29 11:21:22 +0300

    part1s3

--- return.py
+++ return.py
@@ -6,15 +6,26 @@
 #     print(score)
 # update_score()
 # print(score)
-name = "agent"
-level = 2
-
-def show_info():
-    name = "spy"
-    level =4
-    power = level * 10
-    print(name)
-    print(power)
-show_info()
-print(name)
-print(level)
+# name = "agent"
+# level = 2
+# #כמו לפני הוא מדפיס את מה שבתוך הפונקציה וההדפסה בחוץ 
+# #מדפיסה את מה שמחוץ לפונקציה
+# def show_info():
+#     name = "spy"
+#     level =4
+#     power = level * 10
+#     print(name)
+#     print(power)
+# show_info()
+# print(name)
+# print(level)
+#3
+#זה הדפיס לי 30 מכיוון שבתןך הפרמ היה 5 והוספתי 10 והכפלתי ב2 
+#והשני פשןט מדפיס את הגלןבל
+coins = 20 
+def mission_reward(coins):
+    coins = coins +10
+    coins = coins * 2
+    print(coins)
+mission_reward(5)
+print(coins)
